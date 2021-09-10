b1792b22633cf388900d7b48b0302752a2af1d3a
jocelyn pyw <jocelynwotcheu2@gmail.com>
2021-09-10 22:21:10 +0100

    je veux realiser des modification pour donner a chaques sous categories un immage

--- frontend/src/Screens/CategorieScreen.js
+++ frontend/src/Screens/CategorieScreen.js
@@ -1,21 +1,39 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import dataCategorie from '../Data/CategorieData';
+import '../Style/CategorieScreen.css';
 
 
 function  CategorieScreen (props){
-    console.log(props);
+   
     const data= dataCategorie.categorie.find((x)=>x._id===props.match.params.id);
+    const dataC=dataCategorie.categorie;
     if(!data){
         return<div>Categorie Not Found</div>
     }
+    console.log(dataCategorie.categorie);
+    console.log('le type de dataCategorie categorie et est de '+ typeof(dataCategorie.categorie));
     
     
     return(
-        <>
-        <p>Tous se passe bien lorem CategorieScreenCategorieScreenCategorieScreenCategorieScreenCategorieScreen
-        CategorieScreenCategorieScreenCategorieScreenCategorieScreenCategorieScreen
-        </p>
-        </>
+        <Fragment>
+            <h1 className="title">{data.name}</h1>
+            <div className="container">
+                <div className="row">
+                    {
+                       data.sousCategories.map((sousCategories,index)=>(
+                           <div className="col-xs-12 col-md-3 box-categorie" key={index}>
+                               <h1 className="title-sous-categories">{sousCategories}</h1>
+
+                           </div>
+                       ))
+                    }
+                 
+                   
+
+                </div>
+
+            </div>
+        </Fragment>
         
     )
 
